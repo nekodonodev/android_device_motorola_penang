78596b0e98c56f4a43172752a9d065323858b25a
lahaina <nattolecats@gmail.com>
2024-03-05 00:39:44 +0900

    penang: Felica

--- device.mk
+++ device.mk
@@ -124,6 +124,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Felica
+-include vendor/felica/config.mk
+
 # Perf
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
